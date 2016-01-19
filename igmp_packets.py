05ba7fce6bf98c73f170dd805b57f893a5cd53e1
Vesa Kuokkanen <vesa.kuokkanen@coriant.com>
2016-01-19 13:18:40 +0200

    initial commit

---
+++ igmp_packets.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+
+from socket import *
+from struct import *
+from itertools import *
+from time import *
+import sys
+import IN
+import threading
+import signal
+
+IGMP_EXCLUDE = 0x04
+dst = '224.0.0.22'
+
+#todo
+def is_ipv4_mc(mcg):
+    return True
+
+def carry_around_add(a, b):
+    c = a + b
+    return (c & 0xffff) + (c >> 16)
+
+def checksum(msg):
+    s = 0
+    for i in range(0, len(msg), 2):
+        w = ord(msg[i]) + (ord(msg[i+1]) << 8)
+        s = carry_around_add(s, w)
+    return ~s & 0xffff
+
+def update_igmp_checksum(pkt):
+    cs = checksum(pkt)
+    #print 'igmp checksum: ' + str(hex(cs))
+    m = []
+    for x in pkt:        
+        m.append(ord(x))
+    higher = (cs >> 8) & 0xff
+    lower = cs & 0xff
+    m[2] = lower
+    m[3] = higher
+    m = pack("%dB" % len(m), *m)
+    return m
+
+def update_ip_checksum(pkt):
+    cs = checksum(pkt)
+    #print 'ip hdr checksum: ' + str(hex(cs))
+    m = []
+    for x in pkt:        
+        m.append(ord(x))
+    higher = (cs >> 8) & 0xff
+    lower = cs & 0xff
+    m[10] = lower
+    m[11] = higher
+    m = pack("%dB" % len(m), *m)
+    return m
+
+def mk_ip_hdr(s, d):
+    ip_ihl_len = 0x46 #8 bits
+    ip_dscp = 0xc0 #8 bits
+    ip_hdr_total_len = 0x0028 #16 bits
+    ip_id = 0x0000 #16 bits
+    ip_flags = 0x4000 #16 bits
+    ip_ttl = 1 #8 bits
+    ip_protocol = 0x02 #8 bits
+    ip_cs = 0x0000 #16 bits (should filled by kernel but seems not???)
+    #ip_src #32 bits
+    #ip_dst #32 bits
+    ip_options = 0x94040000 #32 bits
+    #total len 24 bytes
+    ip_header = pack('!BBHHHBBH4s4sI', ip_ihl_len, ip_dscp, ip_hdr_total_len,
+                     ip_id, ip_flags, ip_ttl, ip_protocol, ip_cs, inet_aton(s),
+                     inet_aton(d), ip_options)
+    return ip_header
+                     
+def dump_packet(data):
+    i = 0
+    for x in data:
+        if i == 4:
+            print ''
+            i = 0
+        i += 1
+        sys.stdout.write(' %0.2x' % ord(x))
+    print ''
+
+def mk_igmp_msg(group, src_list):
+    num_of_sources = len(src_list)
+    igmp_type = 0x22 #8 bits, igmp v3
+    igmp_max_resp = 0x00 #8 bits, igmp v3
+    igmp_checksum = 0x0000 #16 bits
+    #igmp_group = 0x00000000 #32 bits
+    igmp_s_qrv = 0x0000 #16 bits
+    igmp_num_of_records = 0x0001 #16 bits
+    igmp_record_type = IGMP_EXCLUDE #8bits (report)
+    igmp_aux_data_len = 0x00 #8bits
+    igmp_num_src = num_of_sources #16 bits
+    igmp_src_list = [] #list of 32 bits addresses
+    igmp_group = 0x00000000 #32 bits
+
+    igmpv3_report = pack('!BBHHHBBH', igmp_type, igmp_max_resp, igmp_checksum,
+                         igmp_s_qrv, igmp_num_of_records, igmp_record_type,
+                         igmp_aux_data_len, igmp_num_src)
+    for a in src_list:
+        igmpv3_report += pack('!4s', inet_aton(a))
+    igmpv3_report += pack('!4s', inet_aton(group))
+    return igmpv3_report
+
+def mk_igmp_join(src, group):
+    ip_hdr = mk_ip_hdr(src,dst)    
+    igmp = mk_igmp_msg(group, [])
+    #print 'igmp msg:'
+    #dump(igmp)
+    igmp = update_igmp_checksum(igmp)
+    p = ip_hdr + igmp
+    p = update_ip_checksum(p)
+    return p
+    
---
+++ join_test.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+
+# send IGMPv3 join msgs to "all IGMPv3 capable routers" (224.0.0.22)
+# host routing must be set up before hand to send the packets
+# out from correct interface. For example, in Linux it could be
+# something like: "route add 224.0.0.0/8 dev <ifname>".
+
+from igmp_packets import *
+import argparse
+
+############ start of editable variables ############
+MAX_NUM_OF_GROUPS = 16000
+############ end of editable variables ##############
+
+dst = '224.0.0.22' # "to all IGMPv3 capable routers"
+slock = threading.Semaphore() # socket is global. For now, only one task per socket though...
+
+parser = argparse.ArgumentParser(description='Send IGMPv3 join packets to 224.0.0.22 (all IGMPv3 routers)', 
+                                 epilog='Example of use: join_test.py -n 100')
+parser.add_argument('-n', '--number', help='number of different mcgs to join. The script will choose these automatically', 
+                    type=int, default=10)
+parser.add_argument('-d', '--delta', help='delay in milliseconds between sent packets', type=int, default=100)
+parser.add_argument('-m', '--mcgroup', help='first address in the IPv4 MC range', type=str, default='225.0.0.1')
+parser.add_argument('-D', '--dump', help='print hex dump of sent packets', action='store_true')
+required = parser.add_argument_group('required named arguments')
+required.add_argument('-s', '--source', help='IGMP message source', type=str, required=True)
+args = parser.parse_args()
+
+if not(args.number in range(1, MAX_NUM_OF_GROUPS)):
+    print 'Error: invalid number of multicast groups. Max number is ' + str(MAX_NUM_OF_GROUPS)
+    sys.exit(1)
+
+if args.source != '':
+    try:        
+        inet_aton(args.source)   
+    except error:
+        print 'Error: invalid source IP address: ' + args.source
+        sys.exit(2)
+
+if not is_ipv4_mc(args.mcgroup):
+    print 'Error: invalid IPv4 multicast address '
+    sys.exit(1)
+
+def signal_handler(signal, frame):
+    global stop
+    print 'Test interrupted by Ctrl+C!'
+    stop = True
+    #sys.exit(0)
+
+class igmp_t(threading.Thread):
+    def run(self):
+        global stop
+        inc = 0
+        a1 = args.mcgroup.split('.')[1]
+        a2 = args.mcgroup.split('.')[2]
+        a3 = args.mcgroup.split('.')[3]
+        for i,j, k in product(range(int(a1),255),range(int(a2),255),range(int(a3),255)):
+            if (not stop):
+                if inc < args.number:
+                    group = '225.{0}.{1}.{2}'.format(i,j,k) 
+                else:
+                    stop = True
+                    break
+                inc = inc + 1
+                print str(inc) + ': Sending IGMP report for group: ' + group
+                igmp_r = mk_igmp_join(args.source, group)
+                if (args.dump == True):
+                    dump_packet(igmp_r)
+                slock.acquire()
+                s.sendto(igmp_r, (dst, 0))
+                slock.release()
+                sleep(float(args.delta)/float(1000))
+            else:
+                break
+
+s = socket( AF_INET, SOCK_RAW, IPPROTO_RAW )
+s.setsockopt(IPPROTO_IP, IP_HDRINCL, 1)
+s.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, 2)
+stop = False
+
+print 'Press Ctrl+C to quit'
+join_thread = igmp_t()
+join_thread.start()
+signal.signal(signal.SIGINT, signal_handler)
+
+# main thread could do something useful too...
+while(not stop):
+    sleep(1)
+
+join_thread.join()
+s.close()
