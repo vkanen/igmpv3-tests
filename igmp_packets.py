c4b1530281776ebf6ba94d38160d74357aabd93d
Vesa Kuokkanen <vesa.kuokkanen@coriant.com>
2016-04-28 13:11:40 +0300

    added support for include- and exclude filter modes

--- igmp_packets.py
+++ igmp_packets.py
@@ -18,7 +18,7 @@ IGMP_LEAVE = 0x17
 IGMP_EXCLUDE = 0x04
 IGMP_INCLUDE = 0x03
 IGMPV3_ALL_ROUTERS = '224.0.0.22'
-ALL_MC_ROUTERS_IN_LAN = '224.0.0.2'
+ALL_MC_ROUTERS_IN_LAN = '224.0.0.2' # for IGMPv2
 
 #todo
 def is_ipv4_mc(mcg):
@@ -63,13 +63,14 @@ def update_ip_checksum(pkt):
 
 def mk_ip_hdr(s, d):
     ip_ihl_len = 0x46 #8 bits
-    ip_dscp = 0xc0 #8 bits
+    #ip_dscp = 0xc0 #8 bits
+    ip_dscp = 0xe0 #8 bits
     ip_hdr_total_len = 0x0028 #16 bits
     ip_id = 0x0000 #16 bits
     ip_flags = 0x4000 #16 bits
     ip_ttl = 1 #8 bits
     ip_protocol = 0x02 #8 bits
-    ip_cs = 0x0000 #16 bits (should filled by kernel but seems not???)
+    ip_cs = 0x0000 #16 bits (not filled by kernel, raw sockets, must be calculated later)
     #ip_src #32 bits
     #ip_dst #32 bits
     ip_options = 0x94040000 #32 bits
@@ -103,15 +104,15 @@ def mk_igmp_msg(msg_type, group, record_type, src_list):
         sys.exit(1)
     return pkt
 
-def mk_igmpv3_join_msg(src, group, src_list):
-    if src_list == []:
-        rec_type = IGMP_EXCLUDE # exclude empty src => all sources (*,G)
+def mk_igmpv3_join_msg(src, group, src_list, filter_mode):
+    if filter_mode == 'exclude':
+        rec_type = IGMP_EXCLUDE
     else:
-        rec_type = IGMP_INCLUDE # include non empty src list => (S,G)
+        rec_type = IGMP_INCLUDE
     pkt = mk_igmp_msg(IGMPV3_REPORT, group, rec_type, src_list) 
     return pkt
 
-def mk_igmpv3_leave_msg(src, group, src_list):
+def mk_igmpv3_leave_msg(src, group, src_list, filter_mode):
     return mk_igmp_msg(IGMPV3_REPORT, group, IGMP_INCLUDE, [])
 
 def mk_igmpv2_join_msg(src, group, src_list):
@@ -127,14 +128,15 @@ def mk_igmpv2_leave_msg(src, group, src_list):
 #group: IPv4 MC group
 #src_list: list of MC sources if source specific MC is used (leave empty, [], for (*,G) entries) or for v2
 ##########################################################################################################
-def mk_igmp_report(igmp_version, src, report_type, group, src_list):
+
+def mk_igmp_report(igmp_version, src, report_type, group, src_list, filter_mode):
     if igmp_version == 'v3':
         dst = IGMPV3_ALL_ROUTERS
         if report_type == 'join':
             mk_igmp = mk_igmpv3_join_msg
         elif report_type == 'leave':
             mk_igmp = mk_igmpv3_leave_msg
-            src_list = []            
+            src_list = []
         else:
             print "unsupported IGMP report type " + report_type + ". Supported values: 'join' and 'lave'" 
             sys.exit(1)
@@ -153,9 +155,19 @@ def mk_igmp_report(igmp_version, src, report_type, group, src_list):
         print "unsupported IGMP version " + igmp_version + ". Supported versions are 'v2' & 'v3'"
         sys.exit(1)
 
-    igmp = mk_igmp(src, group, src_list)
+    if igmp_version == 'v3':
+        igmp = mk_igmp(src, group, src_list, filter_mode)
+    else:
+        igmp = mk_igmp(src, group, src_list)
+
     igmp = update_igmp_checksum(igmp)
     ip_hdr = mk_ip_hdr(src,dst)
     p = ip_hdr + igmp
     p = update_ip_checksum(p)
     return p
+
+def add_vlan_tag(msg, vlan, pri, tpid):
+    m=[]
+    m = pack('!BBH', pri, vlan, 0x0800)
+    m += msg
+    return m
--- igmp_test.py
+++ igmp_test.py
@@ -15,11 +15,13 @@ import datetime
 import os
 
 ############ start of editable variables ############
+VERSION = 1.0                                       #
 MAX_NUM_OF_GROUPS = 16000                           # 
 ETHERNET_HEADER_SIZE = 14                           # assuming no tagging
 ############ end of editable variables ##############
 
 # exitcodes
+IGMP_OK = 0
 IGMP_TEST_INSUFFICIENT_PRIVILEDGES = 126 
 IGMP_TEST_INVALID_ARG = 128
 IGMP_TEST_KILL = 137
@@ -29,27 +31,23 @@ IGMP_TEST_ERROR = 1
 ALL_MC_ROUTERS_IN_LAN = '224.0.0.2'
 ALL_IGMPV3_ROUTERS_IN_LAN = '224.0.0.22'
 
-if os.geteuid() != 0: # accessing system sockets require root priviledges
-    print('Indufficient priviledges to run the script: you need to be a root to execute this. Hint: try sudo ./igmp_test.py')
-    sys.exit(IGMP_TEST_INSUFFICIENT_PRIVILEDGES)
-
 slock = threading.Semaphore() # socket is global. For now, only one task per socket though...
 
-parser = argparse.ArgumentParser(description='Send IGMPv3 join msgs to 224.0.0.22 (all IGMPv3 routers)', 
-                                 epilog='Example of use: igmpv3_test.py -n 100 -s 192.168.1.2 -t join')
+parser = argparse.ArgumentParser(description="Send IGMP report message(s) to simulate client's 'join'- or 'leave' actions to/from a multicast\n group. Default join type (for IGMPv3) is 'for all sources': (*,G)", 
+                                 epilog='Example of use: igmp_test.py -n 100 -s 192.168.1.2 -t join')
 parser.add_argument('-n', '--number', help='number of different mcgs to join. The script will choose these automatically', 
                     type=int, default=1)
 parser.add_argument('-d', '--delta', help='delay in microseconds between sent packets', type=int, default=100)
-parser.add_argument('-m', '--mcgroup', help='first address in the IPv4 MC range', type=str, default='225.0.0.1')
+parser.add_argument('-m', '--mcgroup', help='first address in the IPv4 mcg range', type=str, default='225.0.0.1')
 parser.add_argument('-D', '--dump', help='print hex dump of sent packets', action='store_true')
-parser.add_argument('-l', '--list_of_srcs', help='source address list for (S,G) entries', nargs='+', default=[])
+parser.add_argument('-l', '--list_of_srcs', help='source address list for filter-mode: addresses to be included or excluded', type=str, nargs='+', default=[])
+parser.add_argument('-f', '--filter_mode', help='mode type for source address list', type=str, choices=['include','exclude'],default='exclude')
 parser.add_argument('-i', '--igmp_version', help='IGMP version: v2 or v3', type=str, choices=['v2','v3'], default='v3')
 required = parser.add_argument_group('required named arguments')
 required.add_argument('-s', '--source', help='src address of sent packets', type=str, required=True)
 required.add_argument('-t', '--type', help="IGMP report message type", type=str, choices=['join','leave'], required=True)
 
 args = parser.parse_args()
-
 if not(args.number in range(1, MAX_NUM_OF_GROUPS)):
     print 'Error: invalid number of multicast groups. Max number is ' + str(MAX_NUM_OF_GROUPS)
     sys.exit(IGMP_TEST_INVALID_ARG)
@@ -65,8 +63,21 @@ if not is_ipv4_mc(args.mcgroup):
     print 'Error: invalid IPv4 multicast address '
     sys.exit(IGMP_TEST_INVALID_ARG)
 
+def is_ipv4_unicast(a):
+    # todo:
+    return True
+
 def parse_args(args):
-    print 'todo:'
+    if args.filter_mode == 'include' and args.type == 'join' and args.list_of_srcs == []:
+        print "Error: trying to include empty multicast src-list in a join message."
+        sys.exit(IGMP_TEST_INVALID_ARG)
+    if args.igmp_version == 'v2' and args.list_of_srcs != []:
+        print "Error: trying to use multicast source list while IGMPv2 is selected: unsupported action"
+        sys.exit(IGMP_TEST_INVALID_ARG)
+    for s in args.list_of_srcs:
+        if not is_ipv4_mc(s):
+            print "Error: invalid source address in the src address list"
+            sys.exit(IGMP_TEST_INVALID_ARG)
     #todo: check argument validity. E.g. no source-address-lists in "leave" msg etc
 
 def signal_handler(signal, frame):
@@ -76,13 +87,7 @@ def signal_handler(signal, frame):
 
 class igmp_t(threading.Thread):
     def run(self):    
-        if args.type == 'join':
-            src_list = args.list_of_srcs
-        elif args.type == 'leave':
-            src_list = []            
-        else:
-            print 'unsupported IGMP report type: ' + args.type
-            sys.exit(IGMP_TEST_INVALID_ARG)
+        src_list = args.list_of_srcs
         global stop
         inc = 0
         sent_bytes = 0
@@ -93,7 +98,7 @@ class igmp_t(threading.Thread):
         start_time = datetime.datetime.now()
         print 'test start time: ' + str(start_time)
         for i,j, k in product(range(int(a1),255),range(int(a2),255),range(int(a3),255)):
-            if (not stop):
+            if not stop:
                 if inc < args.number:
                     group = a0 + '.{0}.{1}.{2}'.format(i,j,k) 
                 else:
@@ -104,14 +109,16 @@ class igmp_t(threading.Thread):
                 info_s += '{0:41}'.format('Sending IGMP' + args.igmp_version + ' report (' + args.type + ') for group: ')
                 info_s += '{0:15}'.format(group)
                 print info_s
-                igmp_r = mk_igmp_report(args.igmp_version, args.source, args.type, group, src_list)
+                igmp_r = mk_igmp_report(args.igmp_version, args.source, args.type, group, src_list, args.filter_mode)
                 sent_bytes += len(igmp_r) + ETHERNET_HEADER_SIZE
-                if (args.dump == True):
+
+                if args.dump == True:
                     dump_packet(igmp_r)                
-                if (args.igmp_version == '2'):
+                if args.igmp_version == '2':
                     dst = ALL_MC_ROUTERS_IN_LAN
                 else:
                     dst = ALL_IGMPV3_ROUTERS_IN_LAN
+                    
                 slock.acquire()                
                 s.sendto(igmp_r, (dst, 0))
                 slock.release()
@@ -127,6 +134,11 @@ class igmp_t(threading.Thread):
         info_s = str(sent_bytes) + ' bytes sent at rate ' 
         info_s += format('%.3f' % avg_rate) + ' Mbps (avg ' + format('%.0f' % pkts_per_second) + ' packets/s)'
         print info_s
+
+parse_args(args)
+if os.geteuid() != 0: # accessing system sockets require root priviledges
+    print('Indufficient priviledges to run the script: you need to be a root to execute this. Hint: try sudo ./igmp_test.py')
+    sys.exit(IGMP_TEST_INSUFFICIENT_PRIVILEDGES)
 
 s = socket( AF_INET, SOCK_RAW, IPPROTO_RAW )
 s.setsockopt(IPPROTO_IP, IP_HDRINCL, 1)
